b6ce380d6ed1b76153ed6175b768bb73cb103b95
Halley Lucena <halley.lucena@gmail.com>
2022-11-23 12:24:02 +0000

    idem

--- modulefederation.config.js
+++ modulefederation.config.js
@@ -3,7 +3,7 @@ const { dependencies } = require('./package.json')
 module.exports = {
   name: 'host',
   remotes: {
-    remote: 'remote@http://localhost:3002/remoteEntry.js',
+    remote: 'remote@https://ionic-components.vercel.app/remoteEntry.js',
   },
   shared: {
     ...dependencies,
