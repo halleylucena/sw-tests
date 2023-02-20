fc5b9723653088ecff27fe8e125d321f870db4ff
Halley Lucena <30702674+halleylucena@users.noreply.github.com>
2023-02-20 16:59:47 +0000

    Added dynamic module federation

--- modulefederation.config.js
+++ modulefederation.config.js
@@ -1,26 +1,31 @@
-const { dependencies } = require('./package.json')
+const { dependencies } = require("./package.json")
+
+let baseUrl = "base@http://localhost:3003/remoteEntry.js"
+if (process.env.NODE_ENV === "production") {
+    baseUrl = "https://ionic-base.vercel.app/remoteEntry.js"
+}
 
 module.exports = {
-  name: 'host',
-  remotes: {
-    base: 'base@http://localhost:3003/remoteEntry.js'
-  },
-  shared: {
-    ...dependencies,
-    react: {
-      singleton: true,
-      requiredVersion: dependencies['react'],
-      eager: true,
+    name: "host",
+    remotes: {
+        base: baseUrl,
     },
-    'react-dom': {
-      singleton: true,
-      requiredVersion: dependencies['react-dom'],
-      eager: true,
+    shared: {
+        ...dependencies,
+        react: {
+            singleton: true,
+            requiredVersion: dependencies["react"],
+            eager: true,
+        },
+        "react-dom": {
+            singleton: true,
+            requiredVersion: dependencies["react-dom"],
+            eager: true,
+        },
+        "react-query": {
+            singleton: true,
+            requiredVersion: dependencies["react-query"],
+            eager: true,
+        },
     },
-    'react-query': {
-      singleton: true,
-      requiredVersion: dependencies['react-query'],
-      eager: true,
-    },
-  },
 }
